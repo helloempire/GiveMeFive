e973ee57f6fa460ff96c817c10405fa757c6c329
cyy <daniyuu@163.com>
2014-03-16 11:25:28 +0800

    暂时的修改：把暂时的详细程序入口留在了listview上（修改SampleListFragment.java的最后intent）

--- Givemefive/app/src/main/java/com/example/givemefive/app/SampleListFragment.java
+++ Givemefive/app/src/main/java/com/example/givemefive/app/SampleListFragment.java
@@ -4,9 +4,9 @@ package com.example.givemefive.app;
  * Created by cyy on 14-3-15.
  */
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -94,12 +94,19 @@ public class SampleListFragment extends ListFragment {
                 ((MainActivity) getActivity()).showRight();
             }
         });
+
+
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
+
+        //to lijj:改这里进
+        /*
         Intent intent = new Intent(getActivity(), DetailsActivity.class);
         startActivity(intent);
+        */
+        Log.i("list","这是第" + id + "行");
     }
 }
