3c00adc557be83133f69234e00a071a7321792fc
ljj <ljjcode@gmail.com>
2014-04-03 20:02:55 +0800

    增加右边的入口

--- Givemefive/app/src/main/java/com/example/givemefive/app/RightFragment.java
+++ Givemefive/app/src/main/java/com/example/givemefive/app/RightFragment.java
@@ -5,6 +5,7 @@ package com.example.givemefive.app;
  */
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,6 +17,7 @@ public class RightFragment extends Fragment {
 
     private Context context;
     private Button bttestdatabase;
+    private Button buttonSetting;
 
     public RightFragment(Context con){
         context=con;
@@ -25,6 +27,24 @@ public class RightFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.right_fragment, null);
 
+        bttestdatabase = (Button)view.findViewById(R.id.test_database);
+        buttonSetting = (Button)view.findViewById(R.id.buttonSetting);
+
+        bttestdatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),test_database.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(),SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
         
         return view;
     }
