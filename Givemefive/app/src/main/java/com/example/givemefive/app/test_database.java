e4d8763669e781b2dca971df37041a72c756426e
daniyuu <daniyuu@163.com>
2014-04-07 11:05:06 +0800

    登陆入口 http://givemefive.dongyueweb.com/index.php/login/appgenerallogin 相关修改在database里面 但是目前HttpStatus = fail等不到响应

--- Givemefive/app/src/main/java/com/example/givemefive/app/test_database.java
+++ Givemefive/app/src/main/java/com/example/givemefive/app/test_database.java
@@ -3,8 +3,10 @@ package com.example.givemefive.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.apache.http.HttpEntity;
@@ -32,10 +34,15 @@ public class test_database extends Activity {
     private HttpPost httpPost;
     private HttpEntity entity;
 
+    private EditText etusername;
+    private EditText etpassword;
     private TextView tvusername;
     private TextView tvpassword;
     private TextView tvtest;
     private Button get;
+
+    private String username;
+    private String password;
 
     protected void onCreate(Bundle savedInstanceState){
         //解决线程问题
@@ -69,16 +76,29 @@ public class test_database extends Activity {
     public void login(){
         httpClient = new DefaultHttpClient();
         try{
-            httpPost = new HttpPost(url + "/index.php");
-            NameValuePair nameValuePair1 = new BasicNameValuePair("test","test");
+            httpPost = new HttpPost(url + "index.php/login/appgenerallogin");
+
+            username = etusername.getText().toString();
+            password = etpassword.getText().toString();
+            Log.i("testdatabase", "inputusername = " + username);
+            Log.i("testdatabase","inputpassword = " + password);
+
+            //创建一个用户，用于向服务端发送数据时，存放的实体
+            NameValuePair nameValuePair1 = new BasicNameValuePair("username",username);
+            NameValuePair nameValuePair2 = new BasicNameValuePair("password",password);
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
             nameValuePairs.add(nameValuePair1);
+            nameValuePairs.add(nameValuePair2);
+
+            Log.i("testdatabase", "sendusername = " + nameValuePair1);
+            Log.i("testdatabase","sendpassword = " + nameValuePair2);
 
             HttpEntity requesHttpEntity = new UrlEncodedFormEntity(nameValuePairs);
             httpPost.setEntity(requesHttpEntity);
             response = httpClient.execute(httpPost);
 
             if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+                Log.i("testdatabase","HttpStatus = OK");
                 entity = response.getEntity();
                 StringBuffer sb = new StringBuffer();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
@@ -89,30 +109,38 @@ public class test_database extends Activity {
                     sb.append(s);
                 }
 
+                Log.i("testdatabase","S  = " + s);
+
                 JSONObject datas= new JSONObject(sb.toString());
-                String username = null;
-                String password = null;
+                Log.i("testdatabase","datas = " + datas.toString());
+                String status = null;
+                String response = null;
                 String test = null;
 
-                username = datas.getString("username");
-                password = datas.getString("password");
-                test = datas.getString("test");
+                status = datas.getString("status");
+                response = datas.getString("response");
+                test = "login test";
 
-                tvusername.setText(username);
-                tvpassword.setText(password);
+                tvusername.setText(status);
+                tvpassword.setText(response);
                 tvtest.setText(test);
             }
 
         }catch (Exception e){
             System.out.println();
+            Log.i("testdatabase","HttpStatus = fail");
         }
     }
 
     private void initView() {
+        etusername = (EditText) findViewById(R.id.test_et_username);
+        etpassword = (EditText) findViewById(R.id.test_et_password);
         tvusername = (TextView) findViewById(R.id.username);
         tvpassword = (TextView) findViewById(R.id.password);
         tvtest = (TextView) findViewById(R.id.test);
         get = (Button) findViewById(R.id.get);
+
+
     }
 
 }
